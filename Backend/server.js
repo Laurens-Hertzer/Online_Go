37f6e897ace3ac3ff7512a02482fad68cfc98bac
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-03-04 11:45:29 +0100

    missed parameter in constructor

--- Backend/server.js
+++ Backend/server.js
@@ -247,7 +247,7 @@ const games = new Map(); //Map instead of Array for better id handling.
 let gameIdCounter = 0;
 
 class Game {
-    constructor(creatorWs) {
+    constructor(creatorWs, timePerPlayer) {
         this.id = `game_${gameIdCounter++}`;
 
         this.player1 = creatorWs;
